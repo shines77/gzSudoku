2f6407e4ba150dabf44da0855939fe478dd00fb8
XiongHui Guo <gz_shines@msn.com>
2022-01-30 19:39:39 +0800

    一些改进
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- src/gzSudoku/JCZSolve_v5.h
+++ src/gzSudoku/JCZSolve_v5.h
@@ -1279,7 +1279,8 @@ private:
             uint32_t newBand = band & lockedCandidates;
             if (fast_mode || newBand != 0) {
                 assert(newBand != 0);
-                changed = 1;
+                if (newBand != band)
+                    changed = 1;
 
                 uint32_t colCombBits = (newBand | (newBand >> 9U) | (newBand >> 18U)) & kFullRowBits;
 #if JCZ_V5_COMP_COLCOMBBITS
@@ -1348,8 +1349,12 @@ private:
                     }
                 }
                 else if (self == 1) {
-                    state.candidates[digit].bands[peer1] &= colLockedSingleMask;
                     uint32_t colLockedSingleRevMask = ~colLockedSingleMask;
+                    uint32_t peer1_band = state.candidates[digit].bands[peer1];
+                    if ((peer1_band & colLockedSingleRevMask) != 0) {
+                        state.candidates[digit].bands[peer1] &= colLockedSingleMask;
+                        changed = 1;
+                    }
                     uint32_t peer2_band = state.candidates[digit].bands[peer2];
                     if ((peer2_band & colLockedSingleRevMask) != 0) {
                         state.candidates[digit].bands[peer2] &= colLockedSingleMask;
@@ -1357,8 +1362,23 @@ private:
                     }
                 }
                 else {
-                    state.candidates[digit].bands[peer1] &= colLockedSingleMask;
-                    state.candidates[digit].bands[peer2] &= colLockedSingleMask;
+                    uint32_t colLockedSingleRevMask = ~colLockedSingleMask;
+                    uint32_t peer1_band = state.candidates[digit].bands[peer1];
+                    if ((peer1_band & colLockedSingleRevMask) != 0) {
+                        state.candidates[digit].bands[peer1] &= colLockedSingleMask;
+                        uint32_t peer2_band = state.candidates[digit].bands[peer2];
+                        if ((peer2_band & colLockedSingleRevMask) != 0) {
+                            state.candidates[digit].bands[peer2] &= colLockedSingleMask;
+                        }
+                        changed = 1;
+                    }
+                    else {
+                        uint32_t peer2_band = state.candidates[digit].bands[peer2];
+                        if ((peer2_band & colLockedSingleRevMask) != 0) {
+                            state.candidates[digit].bands[peer2] &= colLockedSingleMask;
+                            changed = 1;
+                        }
+                    }
                 }
 #endif
 #endif // JCZ_V5_COMP_COLCOMBBITS
@@ -1372,6 +1392,10 @@ private:
                     this->update_solved_rows<digit, self>(state, newBand, bandSolvedRows);
                     if ((digit & 1) == 0)
                         _updated |= 2;
+                    changed = 1;
+                }
+                else {
+                    changed = (_updated != 0);
                 }
                 updated |= _updated;
             }
