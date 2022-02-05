8bf2ebd65fb01265c7b53048e3cc79f2e9eab619
XiongHui Guo <gz_shines@msn.com>
2022-02-05 13:03:33 +0800

    关闭一些测试
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- src/gzSudoku/SudokuMain.cpp
+++ src/gzSudoku/SudokuMain.cpp
@@ -354,17 +354,15 @@ void run_all_benchmark(const char * filename)
     std::atomic_signal_fence(std::memory_order_release);        // _compile_barrier()
 
 #if defined(NDEBUG)
-#if 1
+#if 0
 //  run_sudoku_test<v4::Solver     >(bm_puzzles, bm_puzzleTotal, "dfs::v4");
     run_sudoku_test<v4a::Solver    >(bm_puzzles, bm_puzzleTotal, "dfs::v4a");
     run_sudoku_test<v4b::Solver    >(bm_puzzles, bm_puzzleTotal, "dfs::v4b");
     run_sudoku_test<v5::Solver     >(bm_puzzles, bm_puzzleTotal, "dfs::v5");
     run_sudoku_test<JCZ::v1::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v1");
 #endif
-//  run_sudoku_test<JCZ::v2::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v2");
     run_sudoku_test<JCZ::v2::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v2");
     run_sudoku_test<JCZ::v3::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v3");
-//  run_sudoku_test<JCZ::v4::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v4");
     run_sudoku_test<JCZ::v4::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v4");
     run_sudoku_test<JCZ::v5::Solver>(bm_puzzles, bm_puzzleTotal, "JCZ::v5");
 #else
